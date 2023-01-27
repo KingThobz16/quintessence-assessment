e0ffd2ae70b82b3d81125198ed4b75ac981276d5
Thobela Metu <thobela.metu@gmail.com>
2023-01-27 10:14:11 +0200

    Code comments

--- src/components/DataTable/dataTable.jsx
+++ src/components/DataTable/dataTable.jsx
@@ -4,16 +4,24 @@ import { saveAs } from 'file-saver';
 import '../tables.css'
 
 const DataTable = ({stockId, stockName}) => {
+    // The initial stockData state which will be updated by the OK response of the json-server API Request.
     const [stockData, setStockData] = useState([]);
 
+    // To retrieve the stocks data from the API endpoint after the component loads.
     useEffect(() => {
+        // Get the stocks data using json-server api endpoint
         axios.get('http://localhost:5001/stockValues')
+        // To update the stocksData state with the response data
             .then(res => setStockData(res.data))
             .catch(err => console.log(err));
     }, []);
 
+    // Filter the data to be exported.
+    // We filter the data by returning only the data with a stock_id which matches the stockId prop
     let filterdData = stockData.filter(({ stock_id }) => stock_id === stockId);
 
+    // To export the data using the npm libary "file-saver"
+    // This function will allow us to stringify the filterdData object and return it as a json file.
     const exportData = () => {
         const jsonExport = JSON.stringify(filterdData);
         const blob = new Blob([jsonExport], {type: "application/json"});
@@ -33,6 +41,7 @@ const DataTable = ({stockId, stockName}) => {
                     </tr>
                 </thead>
                 <tbody>
+                    {/* Iterate through the filtered data and generate a row for each set of information */}
                     {filterdData.map(({date, value}, index) => {
                         return (
                             <tr key={index}>
@@ -45,6 +54,7 @@ const DataTable = ({stockId, stockName}) => {
                 </tbody>
             </table>
         </div>
+        {/* An onclick event listener on the button to execute the exportData function */}
         <button onClick={exportData}>Export As Json</button>
     </>
   )
--- src/components/StockTable/stockTable.jsx
+++ src/components/StockTable/stockTable.jsx
@@ -4,13 +4,17 @@ import DataTable from '../DataTable/dataTable';
 import '../tables.css'
 
 function StockTable() {
+  // Initial state to store the list of stocks, stock id and stock name
   const [stocksList, setStocksList] = useState([]);
   const [stockId, setStockId] = useState(null);
   const [stockName, setStockName] = useState('');
 
+  // To retrieve the stocks list from the API endpoint after the component loads and everytime the stock ID changes.
   useEffect(() => {
+    // Get the stocks data using the json-server api endpoint
     axios.get('http://localhost:5000/Stocks')
-      .then(res => setStocksList(res.data))
+    // Update the stocksList state with the response data
+    .then(res => setStocksList(res.data))
       .catch(err => console.log(err));
     }, [stockId]);
 
@@ -28,8 +32,14 @@ function StockTable() {
                   </tr>
               </thead>
               <tbody>
-                  <tr><td className="search__cell" colSpan="4"><input type="text" className="search__input" placeholder="Search" /></td></tr>
+                  <tr>
+                    <td className="search__cell" colSpan="4">
+                      <input type="text" className="search__input" placeholder="Search" />
+                    </td>
+                  </tr>
+                  {/* iterate through the stocksList data and generate a row for each set of information */}
                   {stocksList.map(({id, stock, industry, sector, currency_code}) => (
+                    // An onclick event listener on the table row to update the stockId and stockName state
                     <tr key={id} onClick={() => {setStockId(id); setStockName(stock)}}>
                         <td>{stock}</td>
                         <td>{industry}</td>
@@ -41,6 +51,8 @@ function StockTable() {
           </table>
       </div>
         {
+          // If the stockId state is true, render the DataTable component
+          // and pass the stockId and stockName as props
           stockId && <DataTable stockId={stockId} stockName={stockName}/>
         }
     </>
